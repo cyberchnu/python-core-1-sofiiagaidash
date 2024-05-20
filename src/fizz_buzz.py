78e7cfa67c6b001ca61af2f6f793176d35a6e6b0
Haidash Sofiia <sofiagaidashka@gmail.com>
2024-05-20 20:21:55 +0300

    3

--- src/fizz_buzz.py
+++ src/fizz_buzz.py
@@ -1,3 +1,7 @@
 def fizz_buzz(param):
-  #Type your code here
-  return
+  if param % 5 == 0 and param % 3 == 0:
+    return "FizzBuzz"
+  if param % 5 == 0:
+    return "Buzz"
+  if param % 3 == 0:
+    return "Fizz"
