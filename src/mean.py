9f0c0acd6d52a4fed632157e4e9c5f0456f53bd5
Haidash Sofiia <sofiagaidashka@gmail.com>
2024-05-21 14:53:53 +0300

    10

--- src/mean.py
+++ src/mean.py
@@ -1,3 +1,12 @@
 def mean(number):
-  # Type your code
-  return 
+  number = str(number)
+  suma = 0
+  for i in number:
+    suma += int(i)
+  return int(suma/len(number))
+
+print(mean(42))
+print(mean(12345))
+print(mean(777))
+
+
